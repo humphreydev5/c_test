549bfe0ac243fe09be5d8837f69e3358b3f86865
humphreydev5 <humphreyikhalea6@gmail.com>
2023-07-20 15:58:38 +0000

    Hello World

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - int main(void)
+ *
+ * Return: 0
+ */
+int main(void)
+{
+		int age;
+		age = 30;
+		/*Write a simple C code here*/
+		printf("Hello World \n");
+		printf("My name is ....and I am %i\n", age);
+		age = 31;
+		printf("Hello World\n");
+		printf("My name is....and I am %i\n", age);
+		return (0);
+}
